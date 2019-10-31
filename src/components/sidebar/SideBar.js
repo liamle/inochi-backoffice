19c4556060e0f815161676e1bc0e04684e03501a
liamfullsnack <liamfullsnack@gmail.com>
2019-10-31 23:04:32 +0700

    edit sidebar

--- src/components/sidebar/SideBar.js
+++ src/components/sidebar/SideBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome, faBriefcase, faPaperPlane, faQuestion, faImage, faCopy } from '@fortawesome/free-solid-svg-icons';
+import { faBoxes, faBriefcase, faPaperPlane, faQuestion, faImage, faCopy } from '@fortawesome/free-solid-svg-icons';
 import SubMenu from './SubMenu';
 import { NavItem, NavLink, Nav } from 'reactstrap';
 import classNames from 'classnames';
@@ -15,59 +15,11 @@ const SideBar = props => (
       <div className="side-menu">
         <Nav vertical className="list-unstyled pb-3">
           <p>Dashboard</p>
-          <SubMenu title="Home" icon={faHome} items={submenus[0]}/>
-          <NavItem>
-            <NavLink tag={Link} to={'/about'}>
-              <FontAwesomeIcon icon={faBriefcase} className="mr-2"/>About
-            </NavLink>
-          </NavItem>
-          <SubMenu title="Pages" icon={faCopy} items={submenus[1]}/>
-          <NavItem>
-            <NavLink tag={Link} to={'/pages'}>
-              <FontAwesomeIcon icon={faImage} className="mr-2"/>Portfolio
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink tag={Link} to={'/faq'}>
-              <FontAwesomeIcon icon={faQuestion} className="mr-2"/>FAQ
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink tag={Link} to={'/contact'}>
-              <FontAwesomeIcon icon={faPaperPlane} className="mr-2"/>Contact
-            </NavLink>
-          </NavItem>
+          <SubMenu title="Product" icon={faBoxes} items={[]}/>
+          <SubMenu title="Categories" icon={faCopy} items={[]}/>
         </Nav>        
       </div>
     </div>
   );
-
-  const submenus = [
-    [
-      {
-        title: "Home 1",
-        target: "Home-1"
-      },
-      {
-        title: "Home 2",
-        target: "Home-2",        
-      },
-      {
-        itle: "Home 3",
-        target: "Home-3",      
-      }
-    ],
-    [
-      {
-        title: "Page 1",
-        target: "Page-1",          
-      },
-      {
-        title: "Page 2",
-        target: "Page-2",        
-      }
-    ]
-  ]
-  
 
 export default SideBar;
